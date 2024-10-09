df4ac04bb3ee05c42f8782ba7402227427aed952
Cyber at UNG <akc27873@gmail.com>
2024-10-09 12:19:17 -0400

    Update web_1.php

--- web_1.php
+++ web_1.php
@@ -1,28 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-   <title>XSS 2</title>
-<link rel="shortcut icon" href="../Resources/hmbct.png" />
-</head>
-<body>
-	
-	 <div style="background-color:#c9c9c9;padding:15px;">
-      <button type="button" name="homeButton" onclick="location.href='../homepage.html';">Home Page</button>
-      <button type="button" name="mainButton" onclick="location.href='xssmainpage.html';">Main Page</button>
-    </div>
-<div align="center">
-   <form method="GET" action="" name="form">
-   <p>Your name:<input type="text" name="username"></p>
-   <input type="submit" name="submit" value="Submit">
-</form>
-	</div>
 <?php
-if (isset($_GET["username"])) {
- 	$user = str_replace("<script>", "",$_GET["username"]);
-	echo "Your name is "."$user";
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+	$target_dir = "uploads/";
+	$target_file = $target_dir . basename($_FILES["file"]["name"]);
+	$uploadOk = 1;
+	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+	$type = $_FILES["file"]["type"];
+	$check = getimagesize($_FILES["file"]["tmp_name"]);
+
+	if($check["mime"] == "image/png" || $check["mime"] == "image/gif"){
+		$uploadOk = 1;
+	}else{
+		$uploadOk = 0;
+		echo "Mime?";
+		echo $check["mime"];
+	} 
+  if($uploadOk == 1){
+      move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
+      echo "File uploaded /uploads/".$_FILES["file"]["name"];
+  }
 }
 ?>
-
-
-</body>
-</html>
