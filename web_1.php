e028b0fbbc927fdc1a47725cfbab0ef48d0acb1b
Cyber at UNG <akc27873@gmail.com>
2024-10-09 12:24:34 -0400

    Update web_1.html

--- web_1.php
+++ web_1.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="shortcut icon" href="../Resources/hmbct.png" />
+</head>
+<body>
+<div style="background-color:#c9c9c9;padding:15px;">
+      <button type="button" name="homeButton" onclick="location.href='../homepage.html';">Home Page</button>
+      <button type="button" name="mainButton" onclick="location.href='fileupl.html';">Main Page</button>
+</div>
+<div align="center">
+<form action="" method="post" enctype="multipart/form-data">
+    <br>
+    <b>Select image : </b> 
+    <input type="file" name="file" id="file" style="border: solid;">
+    <input type="submit" value="Submit" name="submit">
+</form>
+	</div>
 <?php
 
 // Check if image file is a actual image or fake image
@@ -22,3 +40,6 @@ if(isset($_POST["submit"])) {
   }
 }
 ?>
+
+</body>
+</html>
